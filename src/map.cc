zbigniew-chyla/widelands

commit ba954b7bb939300f25054b24320f307e4da26f41

Simplify some code and fix padding and wrapping.
git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2505 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/map.cc
@@ -1365,7 +1365,7 @@ uint Map::calc_distance(const Coords a, const Coords b) const
 	// This means we round UP for even rows, and we round DOWN for odd rows.
 	int lx, rx;
 
-	lx = a.x - ((dist + (~a.y & 1))>>1); // div 2
+	lx = a.x - ((dist + (~a.y & 1)) >> 1); // div 2
 	rx = lx + dist;
 
 	// Allow for wrap-around
@@ -1378,8 +1378,7 @@ uint Map::calc_distance(const Coords a, const Coords b) const
 	// Normal, non-wrapping case
 	if (lx <= rx)
 	{
-		if (b.x < lx)
-		{
+		if (b.x < lx) {
 			int dx1 = lx - b.x;
 			int dx2 = b.x - (rx - m_width);
 			dist += std::min(dx1, dx2);
@@ -1923,10 +1922,7 @@ int Map::findpath
 			Map_Object::WALK_SW, Map_Object::WALK_SE, Map_Object::WALK_E, Map_Object::WALK_NE};
 		const char *direction;
 
-		if ((cur.x+cur.y) & 1)
-			direction = order1;
-		else
-			direction = order2;
+		direction = (cur.x + cur.y) & 1 ? order1 : order2;
 
 		// Check all the 6 neighbours
 		for (uint i = 6; i; i--, direction++) {
@@ -1957,10 +1953,11 @@ int Map::findpath
 				continue;
 
 			// Calculate cost
-			if (flags & fpBidiCost)
-				cost = curpf->real_cost + calc_bidi_cost(Coords(cur.x, cur.y), *direction);
-			else
-				cost = curpf->real_cost + calc_cost(Coords(cur.x, cur.y), *direction);
+			cost =
+				curpf->real_cost
+				+
+				(flags & fpBidiCost) ?
+				calc_bidi_cost(cur, *direction) : calc_cost(cur, *direction);
 
 			if (neighbpf->cycle != m_pathcycle) {
 				// add to open list
@@ -2199,11 +2196,11 @@ Military_Influence Map::calc_influence
 	const Y_Coordinate h = get_height();
 	Military_Influence influence = std::max
 		(std::min
-		 (std::min
-		  (abs(a.x - area.x), abs(a.x - area.x + w)), abs(a.x - area.x - w)),
+		 (std::min(abs(a.x - area.x), abs(a.x - area.x + w)),
+		  abs(a.x - area.x - w)),
 		 std::min
-		 (std::min
-		  (abs(a.y - area.y), abs(a.y - area.y + h)), abs(a.y - area.y - h)));
+		 (std::min(abs(a.y - area.y), abs(a.y - area.y + h)),
+		  abs(a.y - area.y - h)));
 
 	//  This method makes a "parabola" like x^4, but the maxium radius is
 	//  MAX_RADIUS,

commit e10802a46eb57007006ce55c8b862708fe2423ee

Bugfix 1784422: Regression in pathfinding introduced by r2505
Costs for fields were calculated incorrectly due to missing
parenthesis around ternary operator. Make the calculation a lot more
explicit to avoid future related bugs.

Also change the way costs are calculated. Cost estimates during
pathfinding are now always lower bounds, and we no longer use floating
point operations to calculate costs.


git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2555 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/map.cc
@@ -1464,8 +1464,8 @@ int Map::is_neighbour(const Coords start, const Coords end) const
 
 
 #define BASE_COST_PER_FIELD 1800
-#define SLOPE_COST_FACTOR      0.02
-#define SLOPE_COST_STEPS       8
+#define SLOPE_COST_DIVISOR  50
+#define SLOPE_COST_STEPS    8
 
 /*
 ===============
@@ -1481,6 +1481,15 @@ int Map::calc_cost_estimate(const Coords a, const Coords b) const
 }
 
 
+/**
+ * \return a lower bound on the time required to walk from \p a to \p b
+ */
+int Map::calc_cost_lowerbound(const Coords a, const Coords b) const
+{
+	return calc_distance(a, b) * calc_cost(-SLOPE_COST_STEPS);
+}
+
+
 /*
 ===============
 Map::calc_cost
@@ -1491,17 +1500,18 @@ The cost is in milliseconds it takes to walk.
 
 The time is calculated as BASE_COST_PER_FIELD * f, where
 
-f = base + d(Slope)
-d = SLOPE_COST_FACTOR * (Slope + SLOPE_COST_STEPS)*(Slope + SLOPE_COST_STEPS - 1) / 2
-base = 1.0 - d(0)
+f = 1.0 + d(Slope) - d(0)
+d = (Slope + SLOPE_COST_STEPS)*(Slope + SLOPE_COST_STEPS - 1) / (2*SLOPE_COST_DIVISOR)
+
+Note that the actual calculations multiply through by (2*SLOPE_COST_DIVISOR)
+to avoid using floating point numbers in game logic code.
 
 Slope is limited to the range [ -SLOPE_COST_STEPS; +oo [
 ===============
 */
-#define CALC_COST_D(slope) (SLOPE_COST_FACTOR * ((slope) + SLOPE_COST_STEPS) * ((slope) + SLOPE_COST_STEPS - 1) * 0.5)
-#define CALC_COST_BASE     (1.0 - CALC_COST_D(0))
+#define CALC_COST_D(slope) (((slope) + SLOPE_COST_STEPS) * ((slope) + SLOPE_COST_STEPS - 1))
 
-static inline float calc_cost_d(int slope)
+static int calc_cost_d(int slope)
 {
 	if (slope < -SLOPE_COST_STEPS)
 		slope = -SLOPE_COST_STEPS;
@@ -1511,8 +1521,9 @@ static inline float calc_cost_d(int slope)
 
 int Map::calc_cost(int slope) const
 {
-	return static_cast<int>
-		(BASE_COST_PER_FIELD * (CALC_COST_BASE + calc_cost_d(slope)));
+	int f = 2*SLOPE_COST_DIVISOR + calc_cost_d(slope) - CALC_COST_D(0);
+
+	return (BASE_COST_PER_FIELD * f) / (2*SLOPE_COST_DIVISOR);
 }
 
 
@@ -1896,7 +1907,7 @@ int Map::findpath
 	curpf = m_pathfields + (start.field-m_fields);
 	curpf->cycle = m_pathcycle;
 	curpf->real_cost = 0;
-	curpf->estim_cost = calc_cost_estimate(start, end);
+	curpf->estim_cost = calc_cost_lowerbound(start, end);
 	curpf->backlink = Map_Object::IDLE;
 
 	Open.push(curpf);
@@ -1949,18 +1960,16 @@ int Map::findpath
 				continue;
 
 			// Calculate cost
-			cost =
-				curpf->real_cost
-				+
-				(flags & fpBidiCost) ?
-				calc_bidi_cost(cur, *direction) : calc_cost(cur, *direction);
+			const int stepcost = (flags & fpBidiCost) ?
+					calc_bidi_cost(cur, *direction) : calc_cost(cur, *direction);
+
+			cost = curpf->real_cost + stepcost;
 
 			if (neighbpf->cycle != m_pathcycle) {
 				// add to open list
 				neighbpf->cycle = m_pathcycle;
 				neighbpf->real_cost = cost;
-				// use a slightly lower estimate for better accuracy
-				neighbpf->estim_cost = calc_cost_estimate(neighb, end) >> 1;
+				neighbpf->estim_cost = calc_cost_lowerbound(neighb, end);
 				neighbpf->backlink = *direction;
 				Open.push(neighbpf);
 			} else if (neighbpf->cost() > cost+neighbpf->estim_cost) {

src/map.h
@@ -265,6 +265,7 @@ struct Map {
 	int is_neighbour(const Coords, const Coords) const;
 
 	int calc_cost_estimate(const Coords, const Coords) const;
+	int calc_cost_lowerbound(const Coords, const Coords) const;
 	int calc_cost(const int slope) const;
 	int calc_cost(const Coords, const int dir) const;
 	int calc_bidi_cost(const Coords, const int dir) const;