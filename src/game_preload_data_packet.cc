zbigniew-chyla/widelands

commit b3031b6923a7dd18a3aa06a4610b3c4de2eff365

* General cleanup in savegame and new game loading code. * New version of Game preload data packet, which was needed for: * -> Fixing bug #1929269 (Computer + human player when loading network-savegame) * -> Using map/world defined loading screens for savegames.
git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@3297 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/game_preload_data_packet.cc
@@ -20,12 +20,13 @@
 #include "game_preload_data_packet.h"
 
 #include "game.h"
+#include "interactive_player.h"
 #include "map.h"
 #include "profile.h"
 
 namespace Widelands {
 
-#define CURRENT_PACKET_VERSION 1
+#define CURRENT_PACKET_VERSION 2
 
 
 void Game_Preload_Data_Packet::Read
@@ -38,8 +39,17 @@ throw (_wexception)
 		Section & s = prof.get_safe_section("global");
 		int32_t const packet_version = s.get_int("packet_version");
 		if (packet_version == CURRENT_PACKET_VERSION) {
-			m_gametime = s.get_safe_int   ("gametime");
-			m_mapname  = s.get_safe_string("mapname");
+			m_gametime   = s.get_safe_int   ("gametime");
+			m_mapname    = s.get_safe_string("mapname");
+			m_background = s.get_safe_string("background");
+			m_player_nr  = s.get_safe_int   ("player_nr");
+		} else if (packet_version == 1) {
+			m_gametime   = s.get_safe_int   ("gametime");
+			m_mapname    = s.get_safe_string("mapname");
+			m_background = "pics/progress_bg.png";
+			// Of course this is wrong, but at least player 1 is always in game
+			// so widelands won't crash with this setting.
+			m_player_nr  = 1;
 		} else
 			throw wexception("unknown/unhandled version %i", packet_version);
 	} catch (_wexception const & e) {
@@ -61,6 +71,13 @@ throw (_wexception)
 	//  save some kind of header.
 	s.set_int   ("gametime",       game->get_gametime());
 	s.set_string("mapname",        game->map().get_name());  // Name of map
+	s.set_int   ("player_nr",      game->get_ipl()->get_player_number()); // player that saved the game.
+
+	std::string bg(                game->map().get_background());
+	if(bg.empty())
+		bg =                        game->map().get_world_name();
+	s.set_string("background",     bg);
+
 
 	prof.write("preload", false, fs);
 }

src/game.cc
@@ -27,9 +27,9 @@
 #include "interactive_player.h"
 #include "interactive_spectator.h"
 #include "fullscreen_menu_launchgame.h"
-#include "fullscreen_menu_loadgame.h"
 #include "game_loader.h"
 #include "game_tips.h"
+#include "game_preload_data_packet.h"
 #include "gamecontroller.h"
 #include "gamesettings.h"
 #include "graphic.h"
@@ -297,7 +297,7 @@ bool Game::run_splayer_scenario_direct(const char* mapname) {
 /**
  * Initialize the game based on the given settings.
  */
-void Game::init(UI::ProgressWindow & loaderUI, const GameSettings& settings) {
+void Game::init_newgame(UI::ProgressWindow & loaderUI, const GameSettings& settings) {
 	g_gr->flush(PicMod_Menu);
 
 	loaderUI.step(_("Preloading map"));
@@ -355,6 +355,12 @@ void Game::init_savegame(UI::ProgressWindow & loaderUI, const GameSettings& sett
 		std::auto_ptr<FileSystem> const fs
 				(g_fs->MakeSubFileSystem(settings.mapfilename.c_str()));
 		Game_Loader gl(*fs, this);
+
+		Widelands::Game_Preload_Data_Packet gpdp;
+		gl.preload_game(&gpdp);
+		std::string background(gpdp.get_background());
+		loaderUI.set_background(background);
+
 		loaderUI.step(_("Loading..."));
 		gl.load_game(settings.multiplayer);
 	} catch (...) {
@@ -365,20 +371,15 @@ void Game::init_savegame(UI::ProgressWindow & loaderUI, const GameSettings& sett
 
 /**
  * Load a game
- * argument defines if this is a single player game (false)
- * or networked (true)
+ * Returns false if the user cancels the dialog. Otherwise returns the result
+ * of running the game.
  */
-bool Game::run_load_game(bool multiplayer, std::string filename) {
-	if (filename.empty()) {
-		Fullscreen_Menu_LoadGame ssg(*this);
-		if (ssg.run() > 0)
-			filename = ssg.filename();
-		else
-			return false;
-	}
-
+bool Game::run_load_game(std::string filename) {
 	UI::ProgressWindow loaderUI;
 	GameTips tips (loaderUI);
+	int8_t player_nr;
+
+	loaderUI.step(_("Preloading map"));
 
 	// We have to create an empty map, otherwise nothing will load properly
 	set_map(new Map);
@@ -387,14 +388,21 @@ bool Game::run_load_game(bool multiplayer, std::string filename) {
 		std::auto_ptr<FileSystem> const fs
 			(g_fs->MakeSubFileSystem(filename.c_str()));
 
-		set_iabase(new Interactive_Player(*this, 1, !multiplayer, multiplayer)); // FIXME memory leak!!!
-
 		Game_Loader gl(*fs, this);
+
+		Widelands::Game_Preload_Data_Packet gpdp;
+		gl.preload_game(&gpdp);
+		std::string background(gpdp.get_background());
+		loaderUI.set_background(background);
+		player_nr = gpdp.get_player_nr();
+
+		set_iabase(new Interactive_Player(*this, player_nr, true, false));
+
 		loaderUI.step(_("Loading..."));
-		gl.load_game(multiplayer);
+		gl.load_game();
 	}
 
-	set_game_controller(GameController::createSinglePlayer(this, true, 1));
+	set_game_controller(GameController::createSinglePlayer(this, true, player_nr));
 	try {
 		bool ret = run(loaderUI, true);
 		delete m->ctrl;

src/game.h
@@ -93,18 +93,11 @@ struct Game : public Editor_Game_Base {
 	GameController* gameController();
 	void set_write_replay(bool wr);
 	void save_syncstream(bool save);
-	void init(UI::ProgressWindow & loader_ui, const GameSettings& settings);
+	void init_newgame(UI::ProgressWindow & loader_ui, const GameSettings& settings);
 	void init_savegame(UI::ProgressWindow & loader_ui, const GameSettings& settings);
 	bool run_splayer_scenario_direct(const char* mapname);
+	bool run_load_game (std::string filename);
 	bool run (UI::ProgressWindow & loader_ui, bool = false);
-
-	/**
-	 * Loads a game from filename and runs it. If filename is empty, a dialog is
-	 * shown that lets the user chose a file to load. Returns false if the user
-	 * cancels the dialog. Otherwise returns the result of running the game.
-	 */
-	bool run_load_game
-		(const bool multiplayer, std::string filename = std::string());
 
 	virtual void postload();
 	void think();

src/game_preload_data_packet.h
@@ -38,11 +38,15 @@ struct Game_Preload_Data_Packet : public Game_Data_Packet {
 		throw (_wexception);
 
 	char const * get_mapname() {return m_mapname.c_str();}
+	std::string get_background() {return m_background;}
 	uint32_t get_gametime() {return m_gametime;}
+	uint32_t get_player_nr() {return m_player_nr;}
 
 private:
 	std::string m_mapname;
+	std::string m_background;
 	uint32_t m_gametime;
+	uint8_t m_player_nr;
 };
 
 };

src/netclient.cc
@@ -144,7 +144,7 @@ void NetClient::run ()
 		game.set_iabase(ipl);
 		ipl->set_chat_provider(this);
 		if(!d->settings.savegame) // new map
-			game.init(loaderUI, d->settings);
+			game.init_newgame(loaderUI, d->settings);
 		else // savegame
 			game.init_savegame(loaderUI, d->settings);
 		d->time.reset(game.get_gametime());

src/nethost.cc
@@ -305,7 +305,7 @@ void NetHost::run()
 		ipl->set_chat_provider(&d->chat);
 		game.set_iabase(ipl);
 		if(!d->settings.savegame) // new game
-			game.init(loaderUI, d->settings);
+			game.init_newgame(loaderUI, d->settings);
 		else // savegame
 			game.init_savegame(loaderUI, d->settings);
 		d->pseudo_networktime = game.get_gametime();

src/wlapplication.cc
@@ -28,6 +28,7 @@
 #include "fullscreen_menu_fileview.h"
 #include "fullscreen_menu_intro.h"
 #include "fullscreen_menu_launchgame.h"
+#include "fullscreen_menu_loadgame.h"
 #include "fullscreen_menu_loadreplay.h"
 #include "fullscreen_menu_main.h"
 #include "fullscreen_menu_netsetup.h"
@@ -284,7 +285,7 @@ void WLApplication::run()
 	} else if (m_loadgame_filename.size()) {
 		Widelands::Game game;
 		try {
-			game.run_load_game(false, m_loadgame_filename.c_str());
+			game.run_load_game(m_loadgame_filename.c_str());
 		} catch (...) {
 			emergency_save(game);
 			throw;
@@ -1160,53 +1161,19 @@ void WLApplication::mainmenu_singleplayer()
 
 		switch (code) {
 		case Fullscreen_Menu_SinglePlayer::New_Game:
-			new_game();
+			done = new_game();
 			break;
 		case Fullscreen_Menu_SinglePlayer::Load_Game: {
-			Widelands::Game game;
-			try {
-				if (game.run_load_game(true))
-					done = true;
-			} catch (...) {
-				emergency_save(game);
-				throw;
-			}
+			done = load_game();
 			break;
 		}
 		case Fullscreen_Menu_SinglePlayer::Campaign: {
-			Widelands::Game game;
-			std::string filename;
-			for (;;) { // Campaign UI - Loop
-				int32_t campaign;
-				{ //  First start UI for selecting the campaign
-					Fullscreen_Menu_CampaignSelect select_campaign;
-					if (select_campaign.run() > 0)
-						campaign = select_campaign.get_campaign();
-					else // Back was pressed
-						goto end_campaign;
-				}
-				// Than start UI for the selected campaign
-				Fullscreen_Menu_CampaignMapSelect select_campaignmap;
-				select_campaignmap.set_campaign(campaign);
-				if (select_campaignmap.run() > 0) {
-					filename = select_campaignmap.get_map();
-					break;
-				}
-			}
-
-			try {
-				// Load selected campaign-map-file
-				if (game.run_splayer_scenario_direct(filename.c_str()))
-					done = true;
-			} catch (...) {
-				emergency_save(game);
-				throw;
-			}
+			done = campaign_game();
 			break;
 		}
 		default:
 			assert(false);
-		} end_campaign:;
+		}
 	}
 
 }
@@ -1414,7 +1381,7 @@ bool WLApplication::new_game()
 
 			game.set_game_controller(ctrl.get());
 			game.set_iabase(new Interactive_Player(game, 1, false, false));
-			game.init(loaderUI, sp.settings());
+			game.init_newgame(loaderUI, sp.settings());
 			game.run(loaderUI);
 		} catch (...) {
 			emergency_save(game);
@@ -1422,6 +1389,76 @@ bool WLApplication::new_game()
 		}
 	}
 	return true;
+}
+
+
+/**
+ * Handle the "Load game" menu option:
+ * Configure a single player game, care about player position and run it.
+ *
+ * \return @c true if a game was loaded, @c false if the player pressed Back
+ * or aborted the game setup via some other means.
+ */
+bool WLApplication::load_game()
+{
+	Widelands::Game game;
+	std::string filename;
+
+	Fullscreen_Menu_LoadGame ssg(game);
+	if (ssg.run() > 0)
+		filename = ssg.filename();
+	else
+		return false;
+
+	try {
+		if (game.run_load_game(filename))
+			return true;
+	} catch (...) {
+		emergency_save(game);
+		throw;
+	}
+}
+
+
+/**
+ * Handle the "Campaign" menu option:
+ * Show campaign UI, let player select scenario and run it.
+ *
+ * \return @c true if a scenario was played, @c false if the player pressed Back
+ * or aborted the game setup via some other means.
+ */
+bool WLApplication::campaign_game()
+{
+	Widelands::Game game;
+	std::string filename;
+	for (;;) { // Campaign UI - Loop
+		int32_t campaign;
+		{ //  First start UI for selecting the campaign
+			Fullscreen_Menu_CampaignSelect select_campaign;
+			if (select_campaign.run() > 0)
+				campaign = select_campaign.get_campaign();
+			else {// Back was pressed
+				filename = "";
+				break;
+			}
+		}
+		// Than start UI for the selected campaign
+		Fullscreen_Menu_CampaignMapSelect select_campaignmap;
+		select_campaignmap.set_campaign(campaign);
+		if (select_campaignmap.run() > 0) {
+			filename = select_campaignmap.get_map();
+			break;
+		}
+	}
+	try {
+		// Load selected campaign-map-file
+		if (!filename.empty())
+			return game.run_splayer_scenario_direct(filename.c_str());
+	} catch (...) {
+		emergency_save(game);
+		throw;
+	}
+	return false;
 }
 
 

src/wlapplication.h
@@ -184,6 +184,8 @@ struct WLApplication {
 	void mainmenu_editor();
 
 	bool new_game();
+	bool load_game();
+	bool campaign_game();
 	void replay();
 
 #ifdef DEBUG

commit 2a1f16a4e961f9a691e698195db16018c5c5243c

Fix bug #2315808 (regression in r3297: game->get_ipl() dereferenced when null) reported by daniel appert - leo73. Check if the Interactive_Player actually exists before trying to use it. If it does, do what the code tried to do. Otherwise fall back to doing what it did before revision 3297 (writing the packet as version 1).
git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@3529 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/game_preload_data_packet.cc
@@ -66,18 +66,22 @@ throw (_wexception)
 	Profile prof;
 	Section & s = *prof.create_section("global");
 
-	s.set_int   ("packet_version", CURRENT_PACKET_VERSION);
+	Interactive_Player const * const ipl = game->get_ipl();
+
+	s.set_int   ("packet_version", ipl ? CURRENT_PACKET_VERSION : 1);
 
 	//  save some kind of header.
 	s.set_int   ("gametime",       game->get_gametime());
-	s.set_string("mapname",        game->map().get_name());  // Name of map
-	assert(game->get_ipl());
-	s.set_int   ("player_nr",      game->get_ipl()->get_player_number()); // player that saved the game.
+	Map const & map = game->map();
+	s.set_string("mapname",        map.get_name());  // Name of map
+	if (ipl) {
+		s.set_int   ("player_nr",   ipl->get_player_number()); // player that saved the game.
 
-	std::string bg                (game->map().get_background());
-	if (bg.empty())
-		bg =                        game->map().get_world_name();
-	s.set_string("background",     bg);
+		std::string bg             (map.get_background());
+		if (bg.empty())
+			bg =                     map.get_world_name();
+		s.set_string("background",  bg);
+	}
 
 
 	prof.write("preload", false, fs);