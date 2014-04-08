a074a98783f223120ad8e6c1fde43893f0dee925
Tino <tino@geofs.de>
2014-04-08 23:20:22 +0200

    missing spaces for translatable strings

--- world/immovables/cactus4/init.lua
+++ world/immovables/cactus4/init.lua
@@ -2,7 +2,7 @@ dirname = path.dirname(__file__)
 
 world:new_immovable_type{
    name = "cactus5",
-   descname = _"Cactus",
+   descname = _ "Cactus",
    size = "none",
    attributes = {},
    programs = {},
