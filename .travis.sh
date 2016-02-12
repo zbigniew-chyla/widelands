29bf60610e9a71cc9fba3aa13b4acf8427b49cb4
TinoM <tino.miegel@gmail.com>
2016-02-12 14:13:55 +0100

    no framebuffer vor coverity scan
    [skip appveyor]

--- .travis.sh
+++ .travis.sh
@@ -2,24 +2,11 @@ set -ex
 
 # Some of these commands fail transiently. We keep retrying them until they
 # succeed.
-if [ "$CXX" = "g++" ]; then
-   until sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y; do sleep 10; done
-fi
-if [ "$CXX" = "clang++" ]; then
-  until sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-$CLANG_VERSION main"; do sleep 10; done;
-  wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-fi
+until sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y; do sleep 10; done
 until sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y; do sleep 10; done
 until sudo apt-get update -qq --force-yes -y; do sleep 10; done
-
-if [ "$CXX" = "g++" ]; then
-   sudo apt-get install -qq --force-yes -y g++-$GCC_VERSION;
-   export CXX="g++-$GCC_VERSION" CC="gcc-$GCC_VERSION";
-fi
-if [ "$CXX" = "clang++" ]; then
-   sudo apt-get install -qq --force-yes -y clang-$CLANG_VERSION;
-   export CXX="clang++-$CLANG_VERSION" CC="clang-$CLANG_VERSION";
-fi
+until sudo apt-get install -qq --force-yes -y g++-5; do sleep 10; done
+export CXX="g++-5" CC="gcc-5";
 
 until sudo apt-get install -qq --force-yes -y \
    cmake \
